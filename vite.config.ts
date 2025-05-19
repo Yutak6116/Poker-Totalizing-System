eb64c4a9e40fcf74a0cca5187240d1377f964f48
Yutak6116 <121711113+Yutak6116@users.noreply.github.com>
2025-05-19 19:20:01 +0900

    仮デプロイ

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/poker-totalizing-system/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
